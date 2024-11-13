f955c608bf066487ecea0094d5340dd1d8cd32cc
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-11-13 09:03:39 +0900

    #6 .tfファイルをパースしてリソースタイプと名前を出力する

--- src/main/java/com/t_horie/terra_puml/application/domain/service/parser/TerraPumlListener.java
+++ src/main/java/com/t_horie/terra_puml/application/domain/service/parser/TerraPumlListener.java
@@ -1,6 +1,16 @@
 package com.t_horie.terra_puml.application.domain.service.parser;
 
 import com.t_horie.terra_puml.application.service.parser.TerraformBaseListener;
+import com.t_horie.terra_puml.application.service.parser.TerraformParser;
 
 public class TerraPumlListener extends TerraformBaseListener {
+    @Override
+    public void enterResourcetype(TerraformParser.ResourcetypeContext ctx) {
+        System.out.println("enterResourcetype: " + ctx.getText());
+    }
+
+    @Override
+    public void enterName(TerraformParser.NameContext ctx) {
+        System.out.println("enterName: " + ctx.getText());
+    }
 }
