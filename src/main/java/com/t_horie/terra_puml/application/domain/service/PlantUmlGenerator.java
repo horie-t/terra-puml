3ec2cfd49d62622bf7b0b3bbdccced757ab790a1
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-11-14 05:45:41 +0900

    #7 tfファイルのパーサを通す

--- src/main/java/com/t_horie/terra_puml/application/domain/service/PlantUmlGenerator.java
+++ src/main/java/com/t_horie/terra_puml/application/domain/service/PlantUmlGenerator.java
@@ -1,16 +1,28 @@
 package com.t_horie.terra_puml.application.domain.service;
 
+import com.t_horie.terra_puml.application.domain.service.parser.TerraPumlListener;
 import com.t_horie.terra_puml.application.port.in.GeneratePlantUmlUse;
+import com.t_horie.terra_puml.application.service.parser.TerraformLexer;
+import com.t_horie.terra_puml.application.service.parser.TerraformParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Component
 public class PlantUmlGenerator implements GeneratePlantUmlUse {
     @Override
-    public String generateFromTerraform(File path) {
-        var sb = new StringBuilder();
+    public String generateFromTerraform(File path) throws IOException {
+        var is = new FileInputStream(path);
+        var lexer = new TerraformLexer(CharStreams.fromStream(is));
+        var parser = new TerraformParser(new CommonTokenStream(lexer));
+        ParseTreeWalker.DEFAULT.walk(new TerraPumlListener(), parser.file_());
 
+        var sb = new StringBuilder();
         appendStart(sb);
         appendHeader(sb);
         appendResource(sb);
--- src/main/java/com/t_horie/terra_puml/application/port/in/GeneratePlantUmlUse.java
+++ src/main/java/com/t_horie/terra_puml/application/port/in/GeneratePlantUmlUse.java
@@ -1,7 +1,8 @@
 package com.t_horie.terra_puml.application.port.in;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface GeneratePlantUmlUse {
-     String generateFromTerraform(File path);
+     String generateFromTerraform(File path) throws IOException;
 }
