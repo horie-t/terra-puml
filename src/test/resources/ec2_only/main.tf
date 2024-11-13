e7e638d482747c24b89c5e2e1716ce124701b936
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-11-14 06:41:34 +0900

    #8 サンプルのtfファイルにtagsを追加する

--- src/test/resources/ec2_only/main.tf
+++ src/test/resources/ec2_only/main.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "example" {
   ami           = "ami-04dd23e62ed049936"
   instance_type = "t3.micro"
+
+    tags = {
+      Name = "WebServer"
+      asPlantUML = "web"
+    }
 }
