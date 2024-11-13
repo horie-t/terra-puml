24c47fb1cc4456822644664a7549a16d652bf0d6
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-11-13 20:15:41 +0900

    #7 EC2インスタンスだけの固定内容のPlantUMLの図を出力する

--- src/main/java/com/t_horie/terra_puml/adapter/in/cli/TerraPumlRunner.java
+++ src/main/java/com/t_horie/terra_puml/adapter/in/cli/TerraPumlRunner.java
@@ -4,6 +4,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 @Component
 public class TerraPumlRunner implements ApplicationRunner {
     @Override
@@ -12,7 +15,17 @@ public class TerraPumlRunner implements ApplicationRunner {
             System.out.println("from: " + args.getOptionValues("from").get(0));
         }
         if (args.containsOption("to")) {
-            System.out.println("to: " + args.getOptionValues("to").get(0));
+            var outputPath = args.getOptionValues("to").get(0);
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
+                writer.write("""
+                        @startuml
+                        !include <awslib/AWSCommon>
+                        !include <awslib/Compute/EC2>
+                        
+                        EC2(web, "Web Server", "PHP and Apache", "Frontend server")
+                        @enduml
+                        """);
+            }
         }
     }
 }
