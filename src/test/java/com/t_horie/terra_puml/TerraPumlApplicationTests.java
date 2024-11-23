08ac91f91c67bdafb5da1e1b67adca7063697e42
HORIE Tetsuya <horie-t@users.noreply.github.com>
2024-11-24 06:43:03 +0900

    #17 mvn testを成功させる

--- src/test/java/com/t_horie/terra_puml/TerraPumlApplicationTests.java
+++ src/test/java/com/t_horie/terra_puml/TerraPumlApplicationTests.java
@@ -1,13 +1,20 @@
 package com.t_horie.terra_puml;
 
+import com.t_horie.terra_puml.adapter.in.cli.TerraPumlRunner;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class TerraPumlApplicationTests {
 
+	@MockBean
+	private TerraPumlRunner terraPumlRunner;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		Mockito.doNothing().when(terraPumlRunner).run(Mockito.any());
 	}
 
 }
